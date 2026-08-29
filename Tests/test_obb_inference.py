d237b00a6b986a23ea596c10945e38627475051e
yileliu19-eng <yileliu10@gmail.com>
2026-08-29 23:18:46 +0800

    fix:use new way to read .tif

--- Tests/test_obb_inference.py
+++ Tests/test_obb_inference.py
@@ -1,23 +1,41 @@
+import cv2
+import numpy as np
+import rasterio
 from pathlib import Path
 from ultralytics import YOLO
 
 def run_obb_inference():
-    # 1. 自動定位路徑
     current_dir = Path(__file__).resolve().parent
     project_root = current_dir.parent
-    
+
     model_path = project_root / "runs" / "train" / "airport_obb_v1-4" / "weights" / "best.pt"
     test_source = project_root / "data" / "raw" / "som_san_air.tif"
-    
+
     model = YOLO(model_path)
 
+    try:
+        with rasterio.open(str(test_source)) as src:
+            img_array = src.read([1, 2, 3])
+            img_array = np.transpose(img_array, (1, 2, 0))
+
+            if img_array.dtype == np.uint16 or img_array.max() > 255:
+                img_array = (img_array / img_array.max() * 255).astype(np.uint8)
+
+            img_bgr = cv2.cvtColor(img_array, cv2.COLOR_RGB2BGR)
+            
+    except Exception as e:
+        print(f"error: {e}")
+        return
+
     results = model.predict(
-        source=str(test_source),
-        conf=0.3,       # 信心門檻
-        save=True,      # 自動畫上斜框並存檔
+        source=img_bgr,
+        conf=0.3,
+        save=True,
         project=str(project_root / "runs" / "predict"),
         name="airport_obb_result"
     )
     
+    print("Finish")
+
 if __name__ == "__main__":
     run_obb_inference()
